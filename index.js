243bc1ea417314edca6dbd4b66ec8215205120b2
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-18 19:01:30 +0700

    xong

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ app.set('view engine', 'ejs');
 // app.set('views', './views');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (req, res) => res.render('home', { age: 20 }));
 
 app.get('/chao/:name/:age', (req, res) => {
     const { name, age } = req.params;
     // res.send(`Chao ${name}, ${age} tuoi.`);
     res.render('chao', { name, age })
 });
+
+// app.use((err, req, res, next) => {
+//     res.render('loi');
+// });
 
 app.listen(3000, () => console.log('Server started!'));
 
